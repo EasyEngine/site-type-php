dc02106c492ee026498096e43c9f994be8bdbc14
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-09-21 15:00:16 +0530

    Change custom.conf to user.conf
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- src/PHP.php
+++ src/PHP.php
@@ -261,8 +261,8 @@ class PHP extends EE_Site_Command {
 		$site_php_ini            = $site_conf_dir . '/php-fpm/php.ini';
 		$site_src_dir            = $this->site_data['site_fs_path'] . '/app/src';
 		$server_name             = $this->site_data['site_url'];
-		$custom_conf_dest        = $site_conf_dir . '/nginx/custom/custom.conf';
-		$custom_conf_source      = SITE_PHP_TEMPLATE_ROOT . '/config/nginx/custom.conf.mustache';
+		$custom_conf_dest        = $site_conf_dir . '/nginx/custom/user.conf';
+		$custom_conf_source      = SITE_PHP_TEMPLATE_ROOT . '/config/nginx/user.conf.mustache';
 		$process_user            = posix_getpwuid( posix_geteuid() );
 
 		\EE::log( 'Creating PHP site ' . $this->site_data['site_url'] );

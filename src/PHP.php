b94d81e972c608498d66e862aaf2723a34cb5ff4
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-10-23 18:56:38 +0530

    Update configuration of files according to new mounting style
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/PHP.php
+++ src/PHP.php
@@ -283,13 +283,33 @@ class PHP extends EE_Site_Command {
 
 		$site_conf_dir           = $this->site_data['site_fs_path'] . '/config';
 		$site_conf_env           = $this->site_data['site_fs_path'] . '/.env';
-		$site_nginx_default_conf = $site_conf_dir . '/nginx/main.conf';
+		$site_nginx_default_conf = $site_conf_dir . '/nginx/default.conf';
 		$site_php_ini            = $site_conf_dir . '/php-fpm/php.ini';
 		$site_src_dir            = $this->site_data['site_fs_path'] . '/app/htdocs';
 		$server_name             = $this->site_data['site_url'];
 		$custom_conf_dest        = $site_conf_dir . '/nginx/custom/user.conf';
 		$custom_conf_source      = SITE_PHP_TEMPLATE_ROOT . '/config/nginx/user.conf.mustache';
 		$process_user            = posix_getpwuid( posix_geteuid() );
+
+		$volumes = [
+			[ 'name' => 'htdocs', 'path_to_symlink' => $this->site_data['site_fs_path'] . '/app' ],
+			[ 'name' => 'config_nginx', 'path_to_symlink' => dirname( $site_nginx_default_conf ) ],
+			[ 'name' => 'config_php', 'path_to_symlink' => dirname( $site_php_ini ) ],
+			[ 'name' => 'log_nginx', 'path_to_symlink' => $this->site_data['site_fs_path'] . '/logs/nginx' ],
+			[
+				'name'            => 'data_postfix',
+				'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/postfix/spool'
+			],
+		];
+
+		if ( ! empty($this->site_data['db_host']) && 'db' === $this->site_data['db_host'] ) {
+			$volumes[] = [
+				'name'            => 'data_db',
+				'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/db'
+			];
+		}
+
+		\EE\Site\Utils\create_volumes( $this->site_data['site_url'], $volumes );
 
 		\EE::log( 'Creating PHP site ' . $this->site_data['site_url'] );
 		\EE::log( 'Copying configuration files.' );
@@ -323,12 +343,14 @@ class PHP extends EE_Site_Command {
 		try {
 			$this->dump_docker_compose_yml( [ 'nohttps' => true ] );
 			$this->fs->dumpFile( $site_conf_env, $env_content );
+			\EE\Site\Utils\set_postfix_files( $this->site_data['site_url'], $site_conf_dir );
+			\EE\Site\Utils\start_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'postfix' ] );
 			$this->fs->dumpFile( $site_nginx_default_conf, $default_conf_content );
 			$this->fs->copy( $custom_conf_source, $custom_conf_dest );
+			$this->fs->remove( $this->site_data['site_fs_path'] . '/app/html' );
+			$this->fs->remove( $this->site_data['site_fs_path'] . '/config/nginx/conf.d' );
 			$this->fs->dumpFile( $site_php_ini, $php_ini_content );
-
-			\EE\Site\Utils\set_postfix_files( $this->site_data['site_url'], $site_conf_dir );
-
+			\EE::exec( 'docker-compose restart nginx php' );
 			$index_data = [
 				'version'       => 'v' . EE_VERSION,
 				'site_src_root' => $this->site_data['site_fs_path'] . '/app/htdocs',
@@ -353,6 +375,7 @@ class PHP extends EE_Site_Command {
 
 		$filter   = [];
 		$filter[] = $this->site_data['cache_host'];
+		$filter['site_prefix'] = \EE\Site\Utils\get_site_prefix( $this->site_data['site_url'] );
 		if ( 'mysql' === $this->site_data['app_sub_type'] ) {
 			$filter[] = $this->site_data['db_host'];
 		}
@@ -538,7 +561,6 @@ class PHP extends EE_Site_Command {
 			$this->level = 3;
 			$this->configure_site_files();
 
-			\EE\Site\Utils\start_site_containers( $this->site_data['site_fs_path'], $containers );
 			\EE\Site\Utils\configure_postfix( $this->site_data['site_url'], $this->site_data['site_fs_path'] );
 
 			\EE\Site\Utils\create_etc_hosts_entry( $this->site_data['site_url'] );

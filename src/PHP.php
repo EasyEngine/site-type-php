47082d4c00829bf13d0b7633fa83c03c54553fa9
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-11-16 15:00:40 +0530

    Update volume handling and continer booting flow for linux and darwin
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/PHP.php
+++ src/PHP.php
@@ -291,35 +291,6 @@ class PHP extends EE_Site_Command {
 		$custom_conf_source      = SITE_PHP_TEMPLATE_ROOT . '/config/nginx/user.conf.mustache';
 		$process_user            = posix_getpwuid( posix_geteuid() );
 
-		$volumes = [
-			[ 'name' => 'htdocs', 'path_to_symlink' => $this->site_data['site_fs_path'] . '/app' ],
-			[ 'name' => 'config_nginx', 'path_to_symlink' => dirname( dirname( $site_nginx_default_conf ) ) ],
-			[ 'name' => 'config_php', 'path_to_symlink' => dirname( dirname( $site_php_ini ) ) ],
-			[ 'name' => 'log_php', 'path_to_symlink' => $this->site_data['site_fs_path'] . '/logs/php' ],
-			[ 'name' => 'log_nginx', 'path_to_symlink' => $this->site_data['site_fs_path'] . '/logs/nginx' ],
-			[
-				'name'            => 'data_postfix',
-				'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/postfix/spool'
-			],
-			[
-				'name'            => 'ssl_postfix',
-				'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/postfix/ssl'
-			],
-			[
-				'name'            => 'config_postfix',
-				'path_to_symlink' => $this->site_data['site_fs_path'] . '/config/postfix'
-			],
-		];
-
-		if ( ! empty($this->site_data['db_host']) && 'db' === $this->site_data['db_host'] ) {
-			$volumes[] = [
-				'name'            => 'data_db',
-				'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/db'
-			];
-		}
-
-		$this->docker->create_volumes( $this->site_data['site_url'], $volumes );
-
 		\EE::log( 'Creating PHP site ' . $this->site_data['site_url'] );
 		\EE::log( 'Copying configuration files.' );
 
@@ -352,13 +323,19 @@ class PHP extends EE_Site_Command {
 		try {
 			$this->dump_docker_compose_yml( [ 'nohttps' => true ] );
 			$this->fs->dumpFile( $site_conf_env, $env_content );
-			\EE\Site\Utils\start_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'postfix' ] );
+			if ( ! IS_DARWIN ) {
+				\EE\Site\Utils\start_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'postfix' ] );
+			}
 			\EE\Site\Utils\set_postfix_files( $this->site_data['site_url'], $this->site_data['site_fs_path'] . '/services' );
 			$this->fs->dumpFile( $site_nginx_default_conf, $default_conf_content );
 			$this->fs->copy( $custom_conf_source, $custom_conf_dest );
 			$this->fs->remove( $this->site_data['site_fs_path'] . '/app/html' );
 			$this->fs->dumpFile( $site_php_ini, $php_ini_content );
-			\EE\Site\Utils\restart_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'php' ] );
+			if ( IS_DARWIN ) {
+				\EE\Site\Utils\start_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'php', 'postfix' ] );
+			} else {
+				\EE\Site\Utils\restart_site_containers( $this->site_data['site_fs_path'], [ 'nginx', 'php' ] );
+			}
 			$index_data = [
 				'version'       => 'v' . EE_VERSION,
 				'site_src_root' => $this->site_data['site_fs_path'] . '/app/htdocs',
@@ -380,6 +357,124 @@ class PHP extends EE_Site_Command {
 	 */
 	public function dump_docker_compose_yml( $additional_filters = [] ) {
 
+		$site_conf_dir           = $this->site_data['site_fs_path'] . '/config';
+		$site_nginx_default_conf = $site_conf_dir . '/nginx/conf.d/main.conf';
+		$site_php_ini            = $site_conf_dir . '/php/php/php.ini';
+
+		$volumes = [
+			'nginx'   => [
+				[
+					'name'            => 'htdocs',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/app',
+					'container_path'  => '/var/www',
+				],
+				[
+					'name'            => 'config_nginx',
+					'path_to_symlink' => dirname( dirname( $site_nginx_default_conf ) ),
+					'container_path'  => '/usr/local/openresty/nginx/conf',
+					'skip_darwin'     => true,
+				],
+				[
+					'name'            => 'config_nginx',
+					'path_to_symlink' => $site_nginx_default_conf,
+					'container_path'  => '/usr/local/openresty/nginx/conf/conf.d/main.conf',
+					'skip_linux'      => true,
+					'skip_volume'     => true,
+				],
+				[
+					'name'            => 'log_nginx',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/logs/nginx',
+					'container_path'  => '/var/log/nginx',
+				],
+			],
+			'php'     => [
+				[
+					'name'            => 'htdocs',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/app',
+					'container_path'  => '/var/www',
+				],
+				[
+					'name'            => 'config_php',
+					'path_to_symlink' => dirname( dirname( $site_php_ini ) ),
+					'container_path'  => '/usr/local/etc',
+					'skip_darwin'     => true,
+				],
+				[
+					'name'            => 'config_php',
+					'path_to_symlink' => $site_php_ini,
+					'container_path'  => '/usr/local/etc/php/php/php.ini',
+					'skip_linux'      => true,
+					'skip_volume'     => true,
+				],
+				[
+					'name'            => 'log_php',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/logs/php',
+					'container_path'  => '/var/log/php',
+				],
+
+			],
+			'postfix' => [
+				[
+					'name'            => '/dev/log',
+					'path_to_symlink' => '/dev/log',
+					'container_path'  => '/dev/log',
+					'skip_volume'     => true,
+					'skip_darwin'     => true,
+				],
+				[
+					'name'            => 'data_postfix',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/postfix/spool',
+					'container_path'  => '/var/spool/postfix',
+				],
+				[
+					'name'            => 'ssl_postfix',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/postfix/ssl',
+					'container_path'  => '/etc/ssl/postfix',
+				],
+				[
+					'name'            => 'config_postfix',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/config/postfix',
+					'container_path'  => '/etc/postfix',
+					'skip_darwin'     => true,
+				],
+			],
+		];
+
+		if ( ! empty( $assoc_args['with-db'] ) && 'db' === $this->site_data['db_host'] ) {
+			$volumes['db'] = [
+				[
+					'name'            => 'db_data',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/mariadb/data',
+					'container_path'  => '/var/lib/mysql',
+				],
+				[
+					'name'            => 'db_conf',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/mariadb/conf',
+					'container_path'  => '/etc/mysql',
+					'skip_darwin'     => true,
+				],
+				// [
+				// 	// TODO: Generate Template.
+				// 	'name'            => '',
+				// 	'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/mariadb/conf/my.cnf',
+				// 	'container_path'  => '/etc/mysql/my.cnf',
+				// 	'skip_linux'      => true,
+				// 	'skip_volume'     => true,
+				// ],
+				[
+					'name'            => 'db_logs',
+					'path_to_symlink' => $this->site_data['site_fs_path'] . '/services/mariadb/logs',
+					'container_path'  => '/var/log/mysql',
+				],
+			];
+		}
+
+		if ( ! IS_DARWIN && empty( $this->docker->get_volumes_by_label( $this->site_data['site_url'] ) ) ) {
+			foreach ( $volumes as $volume ) {
+				$this->docker->create_volumes( $this->site_data['site_url'], $volume );
+			}
+		}
+
 		$site_docker_yml = $this->site_data['site_fs_path'] . '/docker-compose.yml';
 
 		$filter                = [];
@@ -395,7 +490,7 @@ class PHP extends EE_Site_Command {
 		}
 
 		$site_docker            = new Site_PHP_Docker();
-		$docker_compose_content = $site_docker->generate_docker_compose_yml( $filter );
+		$docker_compose_content = $site_docker->generate_docker_compose_yml( $filter, $volumes );
 		$this->fs->dumpFile( $site_docker_yml, $docker_compose_content );
 	}
 

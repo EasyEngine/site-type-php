ebb3d6fbd10e3c2001b26b13e9df675e20f15494
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-11-30 18:04:08 +0530

    Avoiding function nesting
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Site_PHP_Docker.php
+++ src/Site_PHP_Docker.php
@@ -96,7 +96,8 @@ class Site_PHP_Docker {
 			],
 		];
 
-		if ( ! empty ( array_intersect( [ GLOBAL_DB, GLOBAL_REDIS ], $filters ) ) ) {
+		$global_network = array_intersect( [ GLOBAL_DB, GLOBAL_REDIS ], $filters );
+		if ( ! empty ( $global_network ) ) {
 			$php['networks']['net'][] = [ 'name' => 'global-backend-network' ];
 		}
 

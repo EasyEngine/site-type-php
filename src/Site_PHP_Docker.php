76f57a5a9dafb2837b26d56968d72e35bc6c5e9d
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-12-04 15:28:17 +0530

    Fix backend network handling
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Site_PHP_Docker.php
+++ src/Site_PHP_Docker.php
@@ -26,6 +26,15 @@ class Site_PHP_Docker {
 			],
 		];
 
+		$network = [
+			'networks_labels' => [
+				'label' => [
+					[ 'name' => 'org.label-schema.vendor=EasyEngine' ],
+					[ 'name' => 'io.easyengine.site=${VIRTUAL_HOST}' ],
+				],
+			],
+		];
+
 		if ( in_array( 'db', $filters, true ) ) {
 			// db configuration.
 			$db['service_name'] = [ 'name' => 'db' ];
@@ -98,7 +107,8 @@ class Site_PHP_Docker {
 
 		$global_network = array_intersect( [ GLOBAL_DB, GLOBAL_REDIS ], $filters );
 		if ( ! empty ( $global_network ) ) {
-			$php['networks']['net'][] = [ 'name' => 'global-backend-network' ];
+			$php['networks']['net'][]          = [ 'name' => 'global-backend-network' ];
+			$network['enable_backend_network'] = true;
 		}
 
 		// nginx configuration.
@@ -134,7 +144,8 @@ class Site_PHP_Docker {
 			],
 		];
 		if ( in_array( GLOBAL_REDIS, $filters, true ) ) {
-			$nginx['networks']['net'][] = [ 'name' => 'global-backend-network' ];
+			$nginx['networks']['net'][]        = [ 'name' => 'global-backend-network' ];
+			$network['enable_backend_network'] = true;
 		}
 		// mailhog configuration.
 		$mailhog['service_name'] = [ 'name' => 'mailhog' ];
@@ -204,15 +215,6 @@ class Site_PHP_Docker {
 				[ 'prefix' => $filters['site_prefix'], 'ext_vol_name' => 'data_postfix' ],
 				[ 'prefix' => $filters['site_prefix'], 'ext_vol_name' => 'ssl_postfix' ],
 				[ 'prefix' => $filters['site_prefix'], 'ext_vol_name' => 'config_postfix' ],
-			],
-		];
-
-		$network = [
-			'networks_labels' => [
-				'label' => [
-					[ 'name' => 'org.label-schema.vendor=EasyEngine' ],
-					[ 'name' => 'io.easyengine.site=${VIRTUAL_HOST}' ],
-				],
 			],
 		];
 

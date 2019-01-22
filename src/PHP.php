ace5c73688fad522ef35b2108307de43b2e56303
Thrijith Thankachan <thrijith13@gmail.com>
2019-01-22 21:00:04 +0530

    Update config creation method for site

--- src/PHP.php
+++ src/PHP.php
@@ -789,11 +789,8 @@ class PHP extends EE_Site_Command {
 			if ( Site::create( $data ) ) {
 				\EE::log( 'Site entry created.' );
 
-				// Get site config files.
-				$files = ConfigHash::get_files_in_path( $this->site_data['site_fs_path'] );
-
 				// Create entries for sites' configuration files.
-				ConfigHash::insert_hash_data( $files, $this->site_data['site_url'] );
+				ConfigHash::create_site_config_hash( $this->site_data['site_fs_path'], $this->site_data['site_url'] );
 
 			} else {
 				throw new \Exception( 'Error creating site entry in database.' );

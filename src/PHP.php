1fa94afbc94dea27861a21d6c4950f96d5e1baf4
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-08-20 14:36:54 +0530

    Phpcs fixes
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- src/PHP.php
+++ src/PHP.php
@@ -338,7 +338,8 @@ class PHP extends EE_Site_Command {
 	 *
 	 */
 	public function info( $args, $assoc_args ) {
-		$format   = \EE\Utils\get_flag_value( $assoc_args, 'format' );
+
+		$format = \EE\Utils\get_flag_value( $assoc_args, 'format' );
 
 		\EE\Utils\delem_log( 'site info start' );
 		if ( ! isset( $this->site_data['site_url'] ) ) {

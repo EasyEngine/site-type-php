28d4c3da016ba98994a06101e357e2d01c18efd4
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-08-20 14:37:27 +0530

    Use return instead of exit
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- src/PHP.php
+++ src/PHP.php
@@ -352,7 +352,7 @@ class PHP extends EE_Site_Command {
 			$site = (array) Site::find( $this->site_data['site_url'] );
 			$site = reset( $site );
 			EE::log( json_encode( $site ) );
-			exit();
+			return;
 		}
 
 		$ssl    = $this->site_data['site_ssl'] ? 'Enabled' : 'Not Enabled';
